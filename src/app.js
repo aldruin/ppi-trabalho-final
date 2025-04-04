2d54f78421940288fffb4574f9fa943ccd618a04
aldruin <abl.souza97@gmail.com>
2025-04-03 22:24:16 -0300

    Registra middlewares de erro

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 import express from "express";
-
+import errorHandler from "./middlewares/ErrorHandler.js";
+import pageNotFoundHandler from "./middlewares/pageNotFoundHandler.js";
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
+
+app.use(pageNotFoundHandler);
+
+app.use(errorHandler);
 
 
 export default app;
